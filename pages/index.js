d972cb96cbcb083d226f13593e421e2935285c6c
Sammy Abukmeil <sammyabukmeil@gmail.com>
2022-09-14 15:44:39 +0100

    Update crossOrigin property

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ export default function Home() {
         <meta name="description" content="Help educators time their lectures" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
           rel="stylesheet"
